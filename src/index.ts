17f20351e8a45097dae49874d5370612efd84139
Oscar Dominguez <dominguez.celada@gmail.com>
2023-01-02 11:57:46 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-import lodash from "lodash";
+import _ from "lodash";
 
-console.log(lodash.merge({ a: 1 }, { b: 2 }));
+console.log(_.merge({ a: 1 }, { b: 2 }));
+console.log(_.defaults({ 'a': 1 }, { 'a': 3, 'b': 2 }));
